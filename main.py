fe3a047532844cf32d047affe094417f9f2a8318
helw02 <helw02@catl.com>
2024-04-24 17:33:03 +0800

    init jjbg

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
+import os
+
 import cv2
+import numpy as np
+from modelscope.outputs import OutputKeys
 from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
-from modelscope.outputs import OutputKeys
+
+UPLOAD_FOLDER = "./upload"
+OUTPUT_FOLDER = "./output"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def convert_image_to_white_background(image: np.ndarray = None):
+    try:
+        if image is None:
+            raise ValueError("Either image_path or image must be provided.")
+
+        if image.shape[2] == 4:
+            alpha_channel = image[:, :, 3]
+            rgb_channels = image[:, :, :3]
+
+            alpha_channel_3d = alpha_channel[:, :, np.newaxis] / 255.0
+            alpha_channel_3d = np.repeat(alpha_channel_3d, 3, axis=2)
+
+            white_background_image = np.ones_like(rgb_channels, dtype=np.uint8) * 255
+
+            foreground = cv2.multiply(rgb_channels, alpha_channel_3d, dtype=cv2.CV_8UC3)
+            background = cv2.multiply(white_background_image, 1 - alpha_channel_3d, dtype=cv2.CV_8UC3)
+
+            processed_img = cv2.add(foreground, background)
+        else:
+            processed_img = image
+        return processed_img
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-universal_matting = pipeline(Tasks.universal_matting, model='damo/cv_unet_universal-matting')
-result = universal_matting('https://modelscope.oss-cn-beijing.aliyuncs.com/demo/image-matting/1.png')
-cv2.imwrite('result.png', result[OutputKeys.OUTPUT_IMG])
+    image_bytes = open("./upload/1.png", "rb").read()
+    img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_UNCHANGED)
+    final_img = convert_image_to_white_background(image=img)
+    if final_img is None:
+        raise Exception()
+    universal_matting = pipeline(Tasks.universal_matting, model='damo/cv_unet_universal-matting')
+    result = universal_matting(final_img)
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, "1.output.png"), result[OutputKeys.OUTPUT_IMG])
+    cv2.imwrite(os.path.join(OUTPUT_FOLDER, "1.output.png"), result[OutputKeys.OUTPUT_IMG][:, :, 3])
+    # result = universal_matting('https://modelscope.oss-cn-beijing.aliyuncs.com/demo/image-matting/1.png')
+    # cv2.imwrite('result.png', result[OutputKeys.OUTPUT_IMG])
